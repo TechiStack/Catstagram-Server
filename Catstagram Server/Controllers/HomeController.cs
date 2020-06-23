1580b84454e085cc0b4b0e899ae744a711c7d403
zEDGE <zedge744@live.com>
2020-06-23 23:41:16 +0500

    Upload with Cat model implementions

---
+++ Catstagram Server/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Catstagram_Server.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+       // [Authorize]
+        public IActionResult Get()
+        {
+
+            return Ok("Its working");
+        }
+
+    }
+}
